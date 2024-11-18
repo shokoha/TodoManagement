7a52338a60ae274ec32991790de580122fe50484
shokoha <thammasorn.khun@mail.kmutt.ac.th>
2024-11-18 14:25:15 +0700

    test2

--- main.js
+++ main.js
@@ -2,6 +2,17 @@ import Todo from './lib/todo'
 import todoManagement from './lib/TodoManagement'
 import { showTodoItem, showNumberOfDone, showNumberOfNotDone } from './UI/todoListUI'
 
+const initEvent = () => {
+    const addBtr = document.querySelector('#addBtn')
+    const addInput = document.querySelector('#addTodo input')
+
+    addBtr.addEventListener('click', () => {
+        if (addInput.value.trim().length = 0) {
+            
+        }
+    })
+}
+
 const todoManager = todoManagement()
 const todoId1 = todoManager.addTodo()
 showTodoItem(todoId1, 'test')
